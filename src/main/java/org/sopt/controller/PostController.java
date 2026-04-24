34dfbe2ee60beea3aa7c7f3815dd2f6fbb6b1a5c
imddoy <imddoy@duksung.ac.kr>
2026-04-24 15:01:02 +0900

    feat: PostController 작성(공통 응답 객체 반영, try-catch문 삭제)

--- src/main/java/org/sopt/controller/PostController.java
+++ src/main/java/org/sopt/controller/PostController.java
@@ -1,8 +1,9 @@
 package org.sopt.controller;
 import java.util.List;
 
-import org.sopt.common.exception.PostNotFoundException;
+import org.sopt.common.response.CustomAPIResponse;
 import org.sopt.dto.request.CreatePostRequest;
+import org.sopt.dto.request.UpdatePostRequest;
 import org.sopt.dto.response.CreatePostResponse;
 import org.sopt.dto.response.PostResponse;
 import org.sopt.service.PostService;
@@ -10,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,51 +24,74 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
 	private PostService postService;
 
-	// POST /posts
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
+
 	@PostMapping
-	public ResponseEntity<CreatePostResponse> createPost(
+	public ResponseEntity<CustomAPIResponse<CreatePostResponse>> createPost(
 		@RequestBody CreatePostRequest request
 	) {
 		CreatePostResponse response = postService.createPost(request);
-		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+		return ResponseEntity.ok(
+			CustomAPIResponse.createSuccess(
+				HttpStatus.CREATED.value(),
+				"게시글 작성 성공",
+				response
+			)
+		);
 	}
 
-	// GET /posts 📝 과제
 	@GetMapping
-	public List<PostResponse> getAllPosts() {
-		return postService.getAllPosts();
+	public ResponseEntity<CustomAPIResponse<List<PostResponse>>> getAllPosts() {
+		List<PostResponse> response = postService.getAllPosts();
+		return ResponseEntity.ok(
+			CustomAPIResponse.createSuccess(
+				HttpStatus.OK.value(),
+				"게시글 목록 조회 성공",
+				response
+			)
+		);
 	}
 
-	// GET /posts/{id} 📝 과제
-	@GetMapping
-	public PostResponse getPost(Long id) {
-		try {
-			return postService.getPost(id);
-		} catch (PostNotFoundException e) {
-			System.out.println("["+e.getErrorCode().getCode()+"] "+ e.getMessage());
-			return null;
-		}
+	@GetMapping("/{id}")
+	public ResponseEntity<CustomAPIResponse<PostResponse>> getPost(@PathVariable Long id) {
+		PostResponse response = postService.getPost(id);
+		return ResponseEntity.ok(
+			CustomAPIResponse.createSuccess(
+				HttpStatus.OK.value(),
+				"게시글 조회 성공",
+				response
+			)
+		);
 	}
 
-	// PUT /posts/{id} 📝 과제
-	@PutMapping
-	public void updatePost(Long id, String newTitle, String newContent) {
-		try{
-			postService.updatePost(id, newTitle, newContent);
-			System.out.println("게시글이 수정되었습니다.");
-		} catch (PostNotFoundException e) {
-			System.out.println("["+e.getErrorCode().getCode()+"] "+ e.getMessage());
-		}
+	@PutMapping("/{id}")
+	public ResponseEntity<CustomAPIResponse<Void>> updatePost(
+		@PathVariable Long id,
+		@RequestBody UpdatePostRequest request
+	) {
+		postService.updatePost(id, request.title(), request.content());
+
+		return ResponseEntity.ok(
+			CustomAPIResponse.createSuccess(
+				HttpStatus.OK.value(),
+				"게시글 수정 성공",
+				null
+			)
+		);
 	}
 
-	// DELETE /posts/{id} 📝 과제
-	@DeleteMapping
-	public void deletePost(Long id) {
-		try {
-			postService.deletePost(id);
-			System.out.println("게시글이 삭제되었습니다.");
-		} catch (PostNotFoundException e) {
-			System.out.println("["+e.getErrorCode().getCode()+"] "+ e.getMessage());
-		}
+	@DeleteMapping("/{id}")
+	public ResponseEntity<CustomAPIResponse<Void>> deletePost(@PathVariable Long id) {
+		postService.deletePost(id);
+
+		return ResponseEntity.ok(
+			CustomAPIResponse.createSuccess(
+				HttpStatus.OK.value(),
+				"리워드 계좌 변경 성공",
+				null
+			)
+		);
 	}
 }
