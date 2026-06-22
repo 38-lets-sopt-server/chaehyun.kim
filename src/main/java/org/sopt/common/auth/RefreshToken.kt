afeaa52f3f0abfa40036a3e5d69c1bf8f74708c2
imddoy <imddoy@duksung.ac.kr>
2026-06-22 16:21:00 +0900

    refactor: refresh token, blacklist 마이그레이션

---
+++ src/main/java/org/sopt/common/auth/RefreshToken.kt
@@ -0,0 +1,33 @@
+package org.sopt.common.auth
+
+import jakarta.persistence.*
+import java.time.LocalDateTime
+
+@Entity
+class RefreshToken private constructor(
+
+    @Column(nullable = false)
+    var userId: Long,
+
+    @Column(
+        nullable = false,
+        unique = true
+    )
+    var token: String,
+
+    @Column(nullable = false)
+    var expiresAt: LocalDateTime
+) {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private val id: Long? = null
+
+    fun rotate(
+        newToken: String,
+        expiresInSeconds: Long
+    ) {
+        token = newToken
+        expiresAt = LocalDateTime.now().plusSeconds(expiresInSeconds)
+    }
+}
---
+++ src/main/java/org/sopt/common/auth/RefreshTokenRepository.kt
@@ -0,0 +1,9 @@
+package org.sopt.common.auth
+
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
+
+interface RefreshTokenRepository : JpaRepository<RefreshToken, Long> {
+    fun findByToken(token: String): RefreshToken?
+    fun deleteByUserId(memberId: Long)
+}
